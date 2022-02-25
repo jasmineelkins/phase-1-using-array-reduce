05d6f46d4bf60d16f09a88b5159be3f996139343
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-02-25 13:35:53 -0500

    commit tree 2578139212a00fbed498f3126bdb082a46cc86d0

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+// Let's say we are hard at work in the battery factory. We've assembled several
+// batches of batteries today. Let's count how many assembled batteries we ended
+// up with.
+
+// * Create a new variable called `totalBatteries`, which holds the sum of all of
+//   the battery amounts in the `batteryBatches` array.
+
 // Code your solution here
+
+function counter(num) {
+  let count = 0;
+  count += num;
+
+  return count;
+}
+
+// const doubledAndSummed = [1, 2, 3].reduce(function (accumulator, element) {
+//   return element * 2 + accumulator;
+// }, 0);
+
+const totalBatteries = batteryBatches.reduce(function (accumulator, element) {
+  return element + accumulator;
+}, 0);
